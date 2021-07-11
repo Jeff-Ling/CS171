a61de18dea9f25e0b7cd93ce383c3796e447589f
Yuhao Song <yuhaos5@uci.edu>
2021-07-12 02:12:43 +0800

    Update MyAI.py

--- Minesweeper_Python/src/MyAI.py
+++ Minesweeper_Python/src/MyAI.py
@@ -97,10 +97,9 @@ class MyAI( AI ):
 			# Ensure action in bound
 			for e in self.needUncover:
 				f = e + [1]
-				
+				"""
 				if e[0] < 0 or e[0] > self.rowDimension or e[1] < 0 or e[1] > self.colDimension or e in self.safeTiles or e in self.hintTiles:
 					self.needUncover.remove(e)
-
 				"""
 				if e[0] < 0 or e[0] > self.rowDimension:
 					self.needUncover.remove(e)
@@ -108,7 +107,6 @@ class MyAI( AI ):
 					self.needUncover.remove(e)
 				elif e in self.safeTiles or e in self.hintTiles:
 					self.needUncover.remove(e)
-				"""
 
 		elif (number >= 1):
 			self.hintTiles.append([self.previousX, self.previousY, number])
