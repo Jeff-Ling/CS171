1c4666eb49cdbc05ea44f26ec17d3894e719dab5
Jiefu Ling <jeffling0603@gmail.com>
2021-07-11 17:52:13 +0800

    Update MyAI.py

--- Minesweeper_Python/src/MyAI.py
+++ Minesweeper_Python/src/MyAI.py
@@ -92,6 +92,7 @@ class MyAI( AI ):
 		if (self.needUncover != list()):
 			self.previousX = self.needUncover[0][0]
 			self.previousY = self.needUncover[0][1]
+			print ([self.previousX, self.previousY])
 			return Action(AI.Action.UNCOVER, self.needUncover[0][0], self.needUncover[0][1])
 		
 
