ccb4879b0806445082b5e0ab1515410fae88652c
Yuhao Song <yuhaos5@uci.edu>
2021-07-21 02:38:11 +0800

    Update MyAI.py

--- Minesweeper_Python/src/MyAI.py
+++ Minesweeper_Python/src/MyAI.py
@@ -334,7 +334,7 @@ class MyAI( AI ):
 				for tile2 in cs2.suspectTile:
 					CS2X = tile2.x + 1
 					CS2Y = tile2.y + 1
-					CS2 += str(CS2X) + str(CS2Y)
+					CS2 += "[" + str(CS2X) + ", " + str(CS2Y) + "]"
 					# CS2 += str([tile2.x + 1, tile2.y + 1])
 				print(CS2)
 
