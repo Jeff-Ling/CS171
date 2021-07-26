d77d4163bc4e4b03ab2e9e21200de123e51f6a75
Jiefu Ling <jeffling0603@gmail.com>
2021-07-27 01:16:54 +0800

    Update MyAI.py

--- Minesweeper_Python/src/MyAI.py
+++ Minesweeper_Python/src/MyAI.py
@@ -259,7 +259,7 @@ class MyAI(AI):
         for neighbour_x in range (x - 1, x + 2):
             for neighbour_y in range (y - 1, y + 2):
                 if 0 <= neighbour_x <= self.rowDimension and 0 <= neighbour_y <= self.colDimension and not(x == neighbour_x and y == neighbour_y):
-                    neighbours.append(self.tiles[self.rowDimension - neighbour_y][neighbour_x])
+                    neighbours.append(self.tiles[self.rowDimension - 1 - neighbour_y][neighbour_x])
 
         return neighbours
     
